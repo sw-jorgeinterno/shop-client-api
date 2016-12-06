53ec87a1af232a542b0bddfabf28fb394dad39a2
Jorge Michael Interno <JorgeInterno@mysale.com>
2016-12-06 17:24:56 +0800

    test

--- impl/getProductsBySearchQueryModelProxy.js
+++ impl/getProductsBySearchQueryModelProxy.js
@@ -14,33 +14,34 @@ module.exports.setup = function(server, proxy) {
    *      description: query string
    *      required: true
    *      type: string
-   *   
+   *
    *    - name: pageNumber
    *      description: number of page to fetch
    *      required: true
    *      type: number
-   *   
+   *
    *    - name: pageSize
    *      description: number of products per page
    *      required: true
    *      type: number
-   *   
+   *
    *    - name: facetFilters
    *      description: facets
    *      required: true
    *      type: array
-   *   
+   *
    *    - name: categories
    *      description: product categories
    *      required: true
    *      type: array
-   *   
-   *   
+   *
+   *
    *   responses:
    *       200: OK
-   *   
+   *
    */
   server.post('/v1/accounts/:accountId/products/', (req, res, next) => {
+    console.log("PROXY!");
     proxy.web(req, res);
     return next();
   });
