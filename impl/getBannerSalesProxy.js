6ec53ff08f5805bb8057d3e5467a23f178ba0b38
Jorge Michael Interno <JorgeInterno@mysale.com>
2016-12-07 18:23:28 +0800

    test commit

---
+++ impl/getBannerSalesProxy.js
@@ -0,0 +1,83 @@
+'use strict';
+
+module.exports.setup = function(server, proxy) {
+
+  /**
+   * @swagger
+   * /ApacHandlers.ashx/GetSalesBanners:
+   *  get:
+   *   description: Proxy endpoint for querying banner sales
+   *   produces:
+   *    - application/json
+   *   parameters:
+   *    - name: saleCategoryID
+   *      description: saleCategoryID
+   *      required: true
+   *      type: string
+   *      in: query
+   *   
+   *    - name: topSalesCount
+   *      description: topSalesCount
+   *      required: true
+   *      type: number
+   *      in: query
+   *   
+   *    - name: getBigImages
+   *      description: getBigImages
+   *      required: true
+   *      type: boolean
+   *      in: query
+   *   
+   *    - name: getPromotion
+   *      description: getPromotion
+   *      required: true
+   *      type: boolean
+   *      in: query
+   *   
+   *    - name: getPriorityStartDate
+   *      description: getPriorityStartDate
+   *      required: true
+   *      type: boolean
+   *      in: query
+   *   
+   *    - name: useOzsaleSize
+   *      description: useOzsaleSize
+   *      required: true
+   *      type: boolean
+   *      in: query
+   *   
+   *    - name: groupNo
+   *      description: groupNo
+   *      required: true
+   *      type: string
+   *      in: query
+   *   
+   *    - name: languageID
+   *      description: languageID
+   *      required: true
+   *      type: string
+   *      in: query
+   *   
+   *    - name: countryID
+   *      description: countryID
+   *      required: true
+   *      type: string
+   *      in: query
+   *   
+   *    - name: userGroup
+   *      description: userGroup
+   *      required: true
+   *      type: string
+   *      in: query
+   *   
+   *   
+   *   responses:
+   *       200: OK
+   *   
+   */
+  server.get('/ApacHandlers.ashx/GetSalesBanners', (req, res, next) => {
+    proxy.web(req, res);
+    return next();
+  });
+
+}
